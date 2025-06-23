6b15eae91136591cce80c384eac89d1e08d69d26
vity24 <vitaly.fsb@gmail.com>
2025-06-23 18:30:19 +0300

    Create test_battle.py

---
+++ tests/test_battle.py
@@ -0,0 +1,23 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from battle import BattleSession
+
+def make_player(id=1):
+    return {
+        'id': id,
+        'name': f'P{id}',
+        'pos': 'F',
+        'points': 50,
+        'country': 'CA',
+        'born': '1990',
+        'weight': '90',
+        'rarity': 'common',
+        'owner_level': 1,
+    }
+
+def test_same_player_ids_across_teams_ok():
+    team1 = [make_player(1), make_player(2)]
+    team2 = [make_player(1), make_player(3)]
+    session = BattleSession(team1, team2)
+    res = session.simulate()
+    assert 'winner' in res
