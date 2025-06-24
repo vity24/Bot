ca85b818780474b0679b2624cba64a3b2a89468b
vity24 <vitaly.fsb@gmail.com>
2025-06-24 19:03:14 +0300

    Update test_battle.py

--- tests/test_battle.py
+++ tests/test_battle.py
@@ -1,6 +1,7 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from battle import BattleSession
+import random
 
 def make_player(id=1):
     return {
@@ -16,6 +17,7 @@ def make_player(id=1):
     }
 
 def test_same_player_ids_across_teams_ok():
+    random.seed(0)
     team1 = [make_player(1), make_player(2)]
     team2 = [make_player(1), make_player(3)]
     session = BattleSession(team1, team2)
