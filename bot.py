bfa0815cc514076116a26c9b9d6d503b84f4cfab
vity24 <vitaly.fsb@gmail.com>
2025-06-25 22:04:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -337,7 +337,7 @@ def _get_random_card_sync():
         'SELECT * FROM cards WHERE rarity=? '
         'AND img NOT LIKE "%default-skater.png%" '
         'AND img NOT LIKE "%default-goalie.png%" '
-        'AND img != "" AND img IS NOT NULL '
+        'AND img != \'\' AND img IS NOT NULL '
         'ORDER BY RANDOM() LIMIT 1',
         (rarity,)
     )
