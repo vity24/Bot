6327b9de7bcc05c2180cf21102a2e53aa34cd44b
vity24 <vitaly.fsb@gmail.com>
2025-06-25 21:06:40 +0300

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,6 +1,10 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-TOKEN = "7649956181:AAErINkWzZJ7BofoorAHxc2fLXMPoaCjkQM"
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
 URL = f"https://Vitaly24.pythonanywhere.com/webhook/{TOKEN}"
 
 res = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={URL}")
